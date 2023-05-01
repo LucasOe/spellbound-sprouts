061b5281f1b1aa82f46f8a80b17ddbc95f6be20a
Markus Essing <essing-markus@web.de>
2023-05-01 21:10:54 +0200

    Make Grid Selectable; Cleanup

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+public GameObject _plane;
+[SerializeField] private int _width, _depth;
+public uint Height = 1;
+    // Start is called before the first frame update
+    void Start() {
+          for (uint x = 0; x < _width; ++x) {
+        for (uint y = 0; y < Height; ++y)
+        {
+            for (uint z = 0; z < _depth; ++z)
+            {
+                if (x > 0 && x < _width - 1 && 
+                    y > 0 && y < Height - 1 && 
+                    z > 0 && z < _depth - 1) 
+                    continue;
+
+                var plane = Instantiate(_plane, new Vector3(x*5,1,z*5), Quaternion.identity);
+            }
+        }
+    } 
     }
 }
--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -4,19 +4,14 @@ using UnityEngine;
  
 public class Plane : MonoBehaviour {
     [SerializeField] private Color _baseColor, _offsetColor;
-    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private MeshRenderer _renderer;
     [SerializeField] private GameObject _highlight;
- 
-    public void Init(bool isOffset) {
-        _renderer.color = isOffset ? _offsetColor : _baseColor;
-    }
  
     void OnMouseEnter() {
         _highlight.SetActive(true);
     }
  
-    void OnMouseExit()
-    {
+    void OnMouseExit() {
         _highlight.SetActive(false);
     }
 }
