85d1ccad15b88c3ca4d835bfb656886942cd424c
LucasOe <lucas.oelker@gmx.de>
2023-05-03 19:53:03 +0200

    Add player rotation to cursor

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,7 +8,7 @@ public class Movement : MonoBehaviour
     [SerializeField]
     private GameObject player;
     [SerializeField]
-    private Camera playerCamera;
+    private new Camera camera;
     [SerializeField]
     private float movementSpeed = 25.0f;
 
@@ -21,7 +21,7 @@ public class Movement : MonoBehaviour
 
     private void Update() {
         // Roatate velocity vector so that the player moves relative to the camera
-        var relativeVelocity = Quaternion.Euler(0, playerCamera.gameObject.transform.eulerAngles.y, 0) * velocity;
+        var relativeVelocity = Quaternion.Euler(0, camera.gameObject.transform.eulerAngles.y, 0) * velocity;
         // Move player
         // transform.Translate(relativeVelocity * movementSpeed * Time.deltaTime);
         rigidbody.MovePosition(transform.position + relativeVelocity * movementSpeed * Time.deltaTime);
@@ -30,5 +30,14 @@ public class Movement : MonoBehaviour
     public void OnMove(InputValue value) {
         var vector = value.Get<Vector2>();
         velocity = new Vector3(vector.x, 0, vector.y);
+    }
+
+    public void OnLook(InputValue value) {
+        var mousePosition = value.Get<Vector2>();
+        var ray = camera.ScreenPointToRay(mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 300f)) {
+            var target = new Vector3(hitInfo.point.x, player.transform.position.y, hitInfo.point.z);
+            player.transform.LookAt(target);
+        }
     }
 }
