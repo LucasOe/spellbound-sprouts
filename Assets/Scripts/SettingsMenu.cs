0d942af1dffdb339087874359107be40f5d5d832
Julz <julian.gkt@gmail.com>
2023-05-07 03:58:28 +0200

    MainMenu, SettingsMenu for Future

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+     public void SetVolume (float volume) {
+        audioMixer.SetFloat("myVolume", volume);
+     }
+
+     public void SetQuality (int qualityIndex) {
+        QualitySettings.SetQualityLevel(qualityIndex);
+     }
+
+     public void SetFullscreen (bool isFullscreen) {
+        Screen.fullScreen = isFullscreen;
+     }
+
+}
