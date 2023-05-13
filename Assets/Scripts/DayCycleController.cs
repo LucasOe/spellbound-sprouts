575529036bbad7d9ed32096f82c805d464026d7f
Julz <julian.gkt@gmail.com>
2023-05-13 22:16:20 +0200

    Day an Night

---
+++ Assets/Scripts/DayCycleController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycleController : MonoBehaviour
+{
+
+    [Range (0,24)] 
+    public float timeOfDay;
+    [SerializeField] private LightingPreset Preset;
+
+    public float orbitSpeed = 1f;
+    public Light sun;
+    public Light moon;
+
+    private bool isNight; 
+
+    void Update()
+    {
+        timeOfDay += Time.deltaTime * orbitSpeed;
+        if (timeOfDay > 24)
+            timeOfDay = 0;
+
+        UpdateLighting(timeOfDay/24f);
+        UpdateTime();
+    }
+
+    private void OnValidate() {
+        UpdateTime();
+    }
+
+    private void UpdateLighting(float timePercent) {
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
+        sun.color = Preset.DirectionalColor.Evaluate(timePercent);  
+    }
+
+    private void UpdateTime()
+    {
+        float alpha = timeOfDay / 24f;
+        float sunRotation = Mathf.Lerp(-90,270,alpha);
+        float moonRotation = sunRotation - 180;
+
+        sun.transform.rotation = Quaternion.Euler(sunRotation, -150f, 0);
+        moon.transform.rotation = Quaternion.Euler(moonRotation, -150f, 0);
+
+        CheckDayNightTransition();
+    }
+
+    private void CheckDayNightTransition() {
+        if (isNight) {
+            if (moon.transform.rotation.eulerAngles.x > 180) {
+                StartDay();
+            }
+        } else {
+            if (sun.transform.rotation.eulerAngles.x > 180) {
+                StartNight();
+            }
+        }
+    }
+
+    private void StartDay(){
+        isNight = false;
+        sun.shadows = LightShadows.Soft;
+        moon.shadows = LightShadows.None;
+    }
+    private void StartNight(){
+        isNight = true;
+        sun.shadows = LightShadows.None;
+        moon.shadows = LightShadows.Soft;
+    }
+}
