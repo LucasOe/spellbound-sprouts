529599b89c0bedc906ed95580be0756f9a70e5dd
LucasOe <lucas.oelker@gmx.de>
2023-06-03 15:40:24 +0200

    Refactor Grid

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-
-    public GameObject Tile;
-
+    public Tile Tile;
     public int Width = 20;
     public int Depth = 20;
-    public GameObject[,] tiles;
+    public Tile[,] tiles;
     
     void Start() {
-        tiles = new GameObject[Width, Depth];
+        tiles = new Tile[Width, Depth];
         for (uint x = 0; x < Width; x++) {
             for (uint z = 0; z < Depth; z++) {
-                GameObject tileInstance = Instantiate(Tile, new Vector3(-22.6f + x * 2, 0.02f, -22.6f + z * 2), Quaternion.identity);
+                Tile tileInstance = Instantiate(Tile, new Vector3(-22.6f + x * 2, 0.02f, -22.6f + z * 2), Quaternion.identity);
+                GameManager gm = Tile.GameManager;
                 tileInstance.name = "Square: " + x + " " + z;
                 tiles[x,z] = tileInstance;
             }
         } 
-        Tile.SetActive(false);
+        Tile.gameObject.SetActive(false);
     }
 }
