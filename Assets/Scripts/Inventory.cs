7246b3a71f88dedbf11a6a9fb86f5dfd49bf9c19
Markus Essing <essing-markus@web.de>
2023-06-29 13:45:49 +0200

    inventory changes

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,38 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
 
-    public ActiveItem activeItem = ActiveItem.Plant;
+    public Tool activeTool = Plant;
     public Plant[] plants;
     public Plant[] herbs;
     public Plant activePlant = null;
     public Plant activeHerb = null;
 
-    public enum ActiveItem
-    {
-        Harvest,
-        Attack,
-        Plant,
-        Herb
-    }
+    //Tools
+    public Tool Harvest = new Tool();
+    public Tool Staff = new Tool();
+    public Tool PlantSeeds = new Tool();
+    public Tool HerbSeeds = new Tool();
 
-    public void SetItem(ActiveItem activeItem)
+    public void SetTool(Tool activeTool)
     {
-        this.activeItem = activeItem;
+        this.activeTool = activeTool;
     }
 
     public void SetItemIndex(int i)
     {
-        this.activePlant = plants[i];
-        this.activeHerb = herbs[i];
+        if(activeTool == PlantSeeds) 
+        {
+            this.activePlant = plants[i];
+        }
+        else if (activeTool == HerbSeeds) 
+        {
+            this.activeHerb = herbs[i];
+        }
     }
 
     public Plant GetPlant()
     {
-        if (activeItem == ActiveItem.Plant)
+        if (activeTool == PlantSeeds)
         {
             return activePlant;
         }
