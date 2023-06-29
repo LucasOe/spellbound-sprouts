5603360dbd7faaedaa4949449f250b7fd54d4e9e
Markus Essing <essing-markus@web.de>
2023-06-29 14:36:56 +0200

    inventory rework

--- Assets/Scripts/Items/Tools/Harvest.cs
+++ Assets/Scripts/Items/Tools/Harvest.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public class Harvest : Tool
 {
-
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Items/Tools/HerbSeeds.cs
+++ Assets/Scripts/Items/Tools/HerbSeeds.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public class HerbSeeds : Tool
 {
-
     // Start is called before the first frame update
     void Start()
     {
